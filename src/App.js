e0c778ed77143b5e7d81c734b5b6a5dfb38940ae
Justvina <johnvict20@gmail.com>
2021-06-14 00:24:22 +0100

    chore: Setup completed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const getData = () => {
+    fetch('https://')
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We are already here now
         </p>
         <a
           className="App-link"
